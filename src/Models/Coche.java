8d6143502cc46cb12048138392f3a8f59a673645
sergio <sergio@MSI>
2017-03-12 11:39:21 +0100

    Estado ventanillas creado

--- src/Models/Coche.java
+++ src/Models/Coche.java
@@ -2,17 +2,29 @@ package Models;
 
 public class Coche extends Vehiculo {
 	private String ventanillas;
-	
-	
-	public Coche(String color, String matricula, String marca, String modelo, int velocidadMaxima,
-			int telefonoDueño) {
+
+	public Coche(String color, String matricula, String marca, String modelo, int velocidadMaxima, int telefonoDueño) {
 		super(color, matricula, marca, modelo, velocidadMaxima, telefonoDueño);
 		subirVentanillas();
 	}
-	public void subirVentanillas(){
-		this.ventanillas="subidas";
+
+	public Coche(String color, String matricula, String marca, String modelo, int velocidadMaxima, int telefonoDueño,
+		 String averia) {
+		super(color, matricula, marca, modelo, velocidadMaxima, telefonoDueño, averia);
+		subirVentanillas();
 	}
-	public void bajarVentanillas(){
-		this.ventanillas="bajadas";
+
+	public void subirVentanillas() {
+		this.ventanillas = "subidas";
+		System.out.println(estadoVentanillas());
+	}
+
+	public void bajarVentanillas() {
+		this.ventanillas = "bajadas";
+		System.out.println(estadoVentanillas());
+	}
+
+	private String estadoVentanillas(){
+		return "Las ventanillas estan: "+this.ventanillas;
 	}
 }
